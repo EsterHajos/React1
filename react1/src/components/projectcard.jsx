035f82d312f9ace9cf0df75d2e32588f960c5fca
EsterHajos <ester.marta.hajos@gmail.com>
2026-05-12 17:00:54 +0200

    added folders started code in card

---
+++ react1/src/components/projectcard.jsx
@@ -0,0 +1,35 @@
+function Component ({
+projectname,
+screenshot,
+githudRepo,
+techUsed,
+writeUp,
+projectfunction,
+learned,
+role, 
+challanges,
+})
+
+return (
+<div className="project-card">
+
+<h2>{projectname}</h2>
+
+<img src={screenshot} alt={projectname} />
+
+<a href={githudRepo}>GitHub Repository link</a>
+
+<p>Tech Used: {techUsed.join(', ')}</p>
+
+<p>About the project: {writeUp}</p>
+
+<p>What the project does: {projectfunction}</p>
+
+<p>What I learned: {learned}</p>
+
+<p>My project role: {role}</p>
+
+<p>Challenges: {challanges}</p>
+
+</div>
+);
