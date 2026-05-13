b840f309fbf45b170d1953111f08e3ca92cf167b
EsterHajos <ester.marta.hajos@gmail.com>
2026-05-13 10:38:18 +0200

    added code working on html

---
+++ react1/src/data/projects.js
@@ -0,0 +1,28 @@
+// Project data for the portfolio
+
+const projects = [
+    {
+    projectname: "Project 1",
+    screenshot: "path/to/screenshot1.png",
+    githudRepo: "link!",
+    techUsed: "React", "Node.js", "Express",
+    writeUp: "This is a project about...",
+    projectfunction: "The project allows users to...",
+    learned: "I learned how to...",
+    role: "I was responsible for...",
+    challanges: "One of the challenges I faced was..."
+    },
+
+    {   
+     projectname: "Project 1",
+    screenshot: "path/to/screenshot1.png",
+    githudRepo: "link!",
+    techUsed: "React", "Node.js", "Express",
+    writeUp: "This is a project about...",
+    projectfunction: "The project allows users to...",
+    learned: "I learned how to...",
+    role: "I was responsible for...",
+    challanges: "One of the challenges I faced was..."
+    },
+
+    export default projects;
